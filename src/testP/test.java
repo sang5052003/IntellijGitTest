53c0a3cbfca225f930fb3a26047322fe05c30292
sangki <sang5052003@rosesystems.kr>
2017-10-18 14:09:28 +0900

    commit test

---
+++ src/testP/test.java
@@ -0,0 +1,11 @@
+package testP;
+
+import org.apache.commons.lang.StringUtils;
+
+public class test {
+
+    public  static void main(String [] args) throws Exception {
+        String name = null;
+        System.out.println(StringUtils.isEmpty(name));
+    }
+}
