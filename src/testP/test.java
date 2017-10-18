ad7d6c53ca8bb3d5c98247fbeddc8ee8c0c05942
sangki <sang5052003@rosesystems.kr>
2017-10-18 14:15:39 +0900

    commit test2

--- src/testP/test.java
+++ src/testP/test.java
@@ -6,6 +6,7 @@ public class test {
 
     public  static void main(String [] args) throws Exception {
         String name = null;
+        String name2 = null;
         System.out.println(StringUtils.isEmpty(name));
     }
 }
