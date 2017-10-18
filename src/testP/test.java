926da845aa6926afad51e0ba0684dfbfa588df58
sangki <sang5052003@rosesystems.kr>
2017-10-18 14:25:21 +0900

    commit test3 clone and push

--- src/testP/test.java
+++ src/testP/test.java
@@ -7,6 +7,7 @@ public class test {
     public  static void main(String [] args) throws Exception {
         String name = null;
         String name2 = null;
+        String name3 = null;
         System.out.println(StringUtils.isEmpty(name));
     }
 }
